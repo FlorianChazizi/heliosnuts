67fbdb4713066cbcca1df638cb6dbef3e3f320d2
FlorianChazizi <chazizi.florian@gmail.com>
2025-01-11 21:10:59 +0200

    a decent try

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
+import dynamic from "next/dynamic";
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
 import Line from "./components/Line";
 import Products from "./components/Products";
-import Map from './components/Map';
+import Map from "./components/Map";
 import Partners from "./components/Partners";
 import ContactUs from "./components/ContactUs";
-import dynamic from "next/dynamic";
-const Navbar = dynamic(() =>  import("./components/Navbar"), { ssr: false });
+
+const Navbar = dynamic(() => import("./components/Navbar"), { ssr: false });
 
 export default function Home() {
-  
   return (
     <div>
       <Navbar />
